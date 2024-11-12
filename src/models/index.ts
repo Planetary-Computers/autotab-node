d9699fa689d1de325286949195ae651b37ad13b9
alexi <chitch@alxi.nl>
2024-11-12 11:46:27 -0500

    v0.0.2

--- src/models/index.ts
+++ src/models/index.ts
@@ -5,7 +5,6 @@ export * from './HTTPValidationError';
 export * from './Input';
 export * from './RunSession';
 export * from './RunSessionState';
-export * from './RunSessionWithData';
 export * from './RunSkillRequest';
 export * from './Skill';
 export * from './ValidationError';
